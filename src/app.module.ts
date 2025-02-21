1c3e0da4979c53d13215e183b7423fb60451a510
urimJ <ufrl128@dgu.ac.kr>
2025-02-22 00:29:32 +0900

    Chore: Configuration 설정 #6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // ConfigModule을 전역 모듈로 설정
+      envFilePath: '.env', // 환경 변수 파일 경로 설정
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
