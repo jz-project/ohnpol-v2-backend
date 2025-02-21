e9e68efe703d67b2a7eb4839188b814e2309b221
urimJ <ufrl128@dgu.ac.kr>
2025-02-22 00:16:35 +0900

    Chore: Swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('Ohnpol')
+    .setDescription('API documentation for Ohnpol')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        in: 'header',
+        name: 'JWT',
+      },
+      'access-token'
+    )
+    .addTag('ohnpol')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory, {
+    swaggerOptions: {
+      // 사전순, method순
+      tagsSorter: 'alpha',
+      operationsSorter: 'method',
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
